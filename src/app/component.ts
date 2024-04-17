fb93af1f59ad7b169f818ab9a54a317e2065add2
AslanHalil <AslanCHalil@outlook.com>
2024-04-17 18:46:14 +0300

    Using writable signals and effects.

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from "@angular/core";
+import {Component, computed, effect, signal} from "@angular/core";
 import {Model} from "./repository.model";
 
 @Component({
@@ -10,28 +10,36 @@ export class ProductComponent {
   private messages = ["Total", "Price"];
   private index = signal<number>(0);
 
-  get count(): number {
-    let result = this.model.getProducts().length;
-    //let total = 0;
-    //for (let i = 0; i < 1000000000; i++) {
-    // total += 1;
-    //}
-    console.log(`count value read: ${result}`);
-    return result;
-  }
+  // get count(): number {
+  //   let result = this.model.getProducts().length;
+  //let total = 0;
+  //for (let i = 0; i < 1000000000; i++) {
+  // total += 1;
+  //}
+  //   console.log(`count value read: ${result}`);
+  //   return result;
+  // }
+
+  count = computed<number>(() => this.model.Products().length);
+  countEffect = effect(() =>
+    console.log(`count value computed: ${this.count()}`));
 
   get total(): string {
-    let result = this.model.getProducts()
+    let result = this.model.Products()
       .reduce((total, p) => total + (p.price ?? 0), 0).toFixed(2);
     console.log(`total value read: ${result}`);
-    return result
-  }
-
-  get message(): string {
-    let result = `${this.messages[this.index()]} $${this.total}`;
-    console.log(`message value read: ${result}`);
     return result;
   }
+
+  // get message(): string {
+  //   let result = `${this.messages[this.index()]} $${this.total}`;
+  //   console.log(`message value read: ${result}`);
+  //   return result;
+  // }
+  message = computed<string>(() =>
+    `${this.messages[this.index()]} $${this.total}`);
+  messageEffect = effect(() =>
+    console.log(`message value computed: ${this.message()}`));
 
   toggleMessage() {
     console.clear();
@@ -43,7 +51,7 @@ export class ProductComponent {
   removeProduct() {
     console.clear();
     console.log("removeProduct method invoked");
-    this.model.deleteProduct(this.model.getProducts()[0].id ?? 0);
+    this.model.deleteProduct(this.model.Products()[0].id ?? 0);
   }
 
 }
