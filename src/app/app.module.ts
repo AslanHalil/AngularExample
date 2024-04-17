c2648a2ce774ffd247079e8c93aed45a7ba6f2da
AslanHalil <AslanCHalil@outlook.com>
2024-04-17 17:19:33 +0300

    Example project for learning Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+import {AppComponent} from './app.component';
+import {ProductComponent} from "./component";
+
+@NgModule({
+  declarations: [
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [ProductComponent]
+})
+export class AppModule {
+}
---
+++ src/app/component.ts
@@ -0,0 +1,34 @@
+import {Component} from "@angular/core";
+import {Model} from "./repository.model";
+
+@Component({
+  selector: "app",
+  templateUrl: "template.html"
+})
+export class ProductComponent {
+  private model: Model = new Model();
+  private messages = ["Total", "Price"];
+  private index = 0;
+
+  get count(): number {
+    return this.model.getProducts().length;
+  }
+
+  get total(): string {
+    return this.model.getProducts()
+      .reduce((total, p) => total + (p.price ?? 0), 0).toFixed(2);
+  }
+
+  get message(): string {
+    return `${this.messages[this.index]} $${this.total}`;
+  }
+
+  toggleMessage() {
+    this.index = (this.index + 1) % 2;
+  }
+
+  removeProduct() {
+    this.model.deleteProduct(this.model.getProducts()[0].id ?? 0);
+  }
+
+}
