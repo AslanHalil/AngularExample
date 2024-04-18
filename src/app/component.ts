8eb8cf27b827d0251654706f0680cdbbdfec9338
AslanHalil <AslanCHalil@outlook.com>
2024-04-18 17:25:17 +0300

    using ngmodel.

--- src/app/component.ts
+++ src/app/component.ts
@@ -17,7 +17,21 @@ export class ProductComponent {
 
   selectedProduct = signal<string | undefined>(undefined);
 
+  get selectedProductProp() {
+    return this.selectedProduct();
+  }
+
+  set selectedProductProp(val) {
+    this.selectedProduct.set(val)
+  };
+
   getSelected(product: Product): boolean {
     return product.name == this.selectedProduct();
+  }
+
+  handleInputEvent(ev: Event) {
+    if (ev.target instanceof HTMLInputElement) {
+      this.selectedProduct.set(ev.target.value);
+    }
   }
 }
