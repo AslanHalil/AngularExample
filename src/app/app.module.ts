c3ba7c223a3022afd0a9f64d2abe49ff39e1f195
AslanHalil <AslanCHalil@outlook.com>
2024-04-20 15:09:38 +0300

    Working with structural iterative directives.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import {ProductComponent} from "./component";
 import {PaAttrDirective} from "./attr.directive";
 import {PaModel} from "./twoway.directive";
 import {PaStructureDirective} from "./structural.directive";
+import {PaIteratorDirective} from "./iterator.directive";
 
 @NgModule({
   declarations: [
-    ProductComponent, PaAttrDirective, PaModel, PaStructureDirective
+    ProductComponent, PaAttrDirective, PaModel, PaStructureDirective, PaIteratorDirective
   ],
   imports: [
     BrowserModule,
