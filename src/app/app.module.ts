0c902099b4409fc8c2023d954ce9d156d41dad92
AslanHalil <AslanCHalil@outlook.com>
2024-04-22 18:30:36 +0300

    listing 16.21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,14 @@ import {PaCellColor} from "./cellColor.directive";
 import {PaCellColorSwitcher} from "./cellColorSwitcher.directive";
 import {ProductTableComponent} from "./productTable.component";
 import {ProductFormComponent} from "./productForm.component";
+import {PaToggleView} from "./toggleView.component";
 
 @NgModule({
   declarations: [
     ProductComponent, PaAttrDirective, PaModel, PaStructureDirective,
     PaIteratorDirective, PaCellColor, PaCellColorSwitcher,
-    ProductTableComponent, ProductFormComponent],
-  imports: [
+    ProductTableComponent, ProductFormComponent, PaToggleView
+  ], imports: [
     BrowserModule,
     FormsModule
   ],
