f8d35e86abde85dfeccd309a4e99cd92febd41f9
AslanHalil <AslanCHalil@outlook.com>
2024-04-22 16:36:14 +0300

    Listing 16.8

--- src/app/productTable.component.ts
+++ src/app/productTable.component.ts
@@ -2,10 +2,10 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: "paProductTable",
-  template: `
-    <div class='bg-info p-2'>
-      This is a multiline template
-    </div>`
+// template: `<div class='bg-info p-2'>
+  // This is a multiline template
+  // </div>`
+  templateUrl: "productTable.component.html"
 })
 export class ProductTableComponent {
 }
