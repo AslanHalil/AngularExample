5b6a5384a325f8f5eb40ceb7ed8ac21c6622d607
AslanHalil <AslanCHalil@outlook.com>
2024-04-22 15:32:20 +0300

    Listing 16.3

---
+++ src/app/productTable.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "paProductTable",
+  template: "<div>This is the table component</div>"
+})
+export class ProductTableComponent {
+}
